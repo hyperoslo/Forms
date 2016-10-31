3ebcca06c73a60a9a58f2a9b2746e361f19989d6
Vadym Markov <markov.vadym@gmail.com>
2016-10-31 15:56:41 +0100

    Validate on update

---
+++ Tests/FORMTextFieldTests.m
@@ -0,0 +1,11 @@
+import UIKit
+import XCTest
+import Form
+
+class FORMTextFieldCellTests: XCTestCase {
+
+    func testIsValid() {
+        let field = FORMField.floatFormFieldWithID("Test")
+        
+    }
+}
